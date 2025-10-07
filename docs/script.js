be1694856a86afaf41b2f5d0206e8eb498accfb8
azmiamru1007-max <azmiamru1007@gmail.com>
2025-10-07 12:47:36 +0700

    Create script.js

---
+++ docs/script.js
@@ -0,0 +1,4 @@
+<button>
+  const tombolsapa = document.getElementById('sapaButton');
+tombolSapa.addEventListener('click, funtion() {
+                            alert('Halo! Terimakasih Sudah Berkunjung");
