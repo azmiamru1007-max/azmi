37e70b1a52dae2fec2e1086f81866ec845c1ecea
azmiamru1007-max <azmiamru1007@gmail.com>
2025-10-07 13:03:25 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-<button>
-  const tombolsapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click, funtion() {
-                            alert('Halo! Terimakasih Sudah Berkunjung");
- });                                  
